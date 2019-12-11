13cb9ea1c69977e3617cdc16f4b25504b1ed2ca2
Leo <losoviz@gmail.com>
2019-12-11 10:23:55 +0800

    Renamed getTypeCollectionName to getTypeName

--- src/TypeResolvers/UserTypeResolver.php
+++ src/TypeResolvers/UserTypeResolver.php
@@ -6,11 +6,11 @@ use PoP\Users\TypeDataResolvers\UserTypeDataResolver;
 
 class UserTypeResolver extends AbstractTypeResolver
 {
-    public const TYPE_COLLECTION_NAME = 'users';
+    public const NAME = 'users';
 
-    public function getTypeCollectionName(): string
+    public function getTypeName(): string
     {
-        return self::TYPE_COLLECTION_NAME;
+        return self::NAME;
     }
 
     public function getId($resultItem)
