9a7bc2cb5d2e5debae8821a12140c073e24608ee
Leo <losoviz@gmail.com>
2020-04-11 17:26:59 +0800

    Moved code from trait to class

--- src/FieldResolvers/RootUserFieldResolver.php
+++ src/FieldResolvers/RootUserFieldResolver.php
@@ -1,19 +1,28 @@
 <?php
 namespace PoP\Users\FieldResolvers;
 
-use PoP\Translation\Facades\TranslationAPIFacade;
-use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 use PoP\API\TypeResolvers\RootTypeResolver;
-use PoP\Users\FieldResolvers\RootUserFieldResolverTrait;
 use PoP\Users\FieldResolvers\AbstractUserFieldResolver;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\Users\TypeResolvers\UserTypeResolver;
 
 class RootUserFieldResolver extends AbstractUserFieldResolver
 {
-    use RootUserFieldResolverTrait;
-
     public static function getClassesToAttachTo(): array
     {
         return array(RootTypeResolver::class);
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return array_merge(
+            parent::getFieldNamesToResolve(),
+            [
+                'user',
+            ]
+        );
     }
 
     public function getSchemaFieldDescription(TypeResolverInterface $typeResolver, string $fieldName): ?string
@@ -24,5 +33,64 @@ class RootUserFieldResolver extends AbstractUserFieldResolver
 			'users' => $translationAPI->__('Users in the current site', 'pop-users'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+			'user' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
+        $translationAPI = TranslationAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'user':
+                return array_merge(
+                    $schemaFieldArgs,
+                    [
+                        [
+                            SchemaDefinition::ARGNAME_NAME => 'id',
+                            SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_ID,
+                            SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The user ID', 'pop-users'),
+                            SchemaDefinition::ARGNAME_MANDATORY => true,
+                        ],
+                    ]
+                );
+        }
+        return $schemaFieldArgs;
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        $cmsusersapi = \PoP\Users\FunctionAPIFactory::getInstance();
+        switch ($fieldName) {
+            case 'user':
+                $query = [
+                    'include' => [$fieldArgs['id']],
+                ];
+                $options = [
+                    'return-type' => POP_RETURNTYPE_IDS,
+                ];
+                if ($users = $cmsusersapi->getUsers($query, $options)) {
+                    return $users[0];
+                }
+                return null;
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
+    {
+        switch ($fieldName) {
+            case 'user':
+                return UserTypeResolver::class;
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName, $fieldArgs);
     }
 }
