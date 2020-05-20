f37218ae5c05025eff0b30a80ed6a2c29c0a3f88
Leonardo Losoviz <leo@getpop.org>
2020-05-20 21:55:53 +0800

    Renamed `init` to `initialize`

--- src/Component.php
+++ src/Component.php
@@ -52,7 +52,7 @@ class Component extends AbstractComponent
     {
         parent::doInitialize();
         self::initYAMLServices(dirname(__DIR__));
-        ServiceConfiguration::init();
+        ServiceConfiguration::initialize();
     }
 
     /**
