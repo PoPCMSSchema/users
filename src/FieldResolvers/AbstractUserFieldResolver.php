4ea244c419ad21bf9297d476180acf9435d9d104
Leonardo Losoviz <leo@getpop.org>
2020-09-02 12:45:08 +0800

    Added object type to $resultItem in `resolveValue`

--- src/FieldResolvers/AbstractUserFieldResolver.php
+++ src/FieldResolvers/AbstractUserFieldResolver.php
@@ -87,8 +87,22 @@ abstract class AbstractUserFieldResolver extends AbstractQueryableFieldResolver
         return parent::getFieldDefaultFilterDataloadingModule($typeResolver, $fieldName, $fieldArgs);
     }
 
-    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
-    {
+    /**
+     * @param array<string, mixed> $fieldArgs
+     * @param array<string, mixed>|null $variables
+     * @param array<string, mixed>|null $expressions
+     * @param array<string, mixed> $options
+     * @return mixed
+     */
+    public function resolveValue(
+        TypeResolverInterface $typeResolver,
+        object $resultItem,
+        string $fieldName,
+        array $fieldArgs = [],
+        ?array $variables = null,
+        ?array $expressions = null,
+        array $options = []
+    ) {
         $cmsusersapi = \PoPSchema\Users\FunctionAPIFactory::getInstance();
         switch ($fieldName) {
             case 'users':
