566ada22a97eb6e16d00b0dea1e014c919e751cf
leoloso <leo@getpop.org>
2021-09-13 08:59:52 +0000

    Merge pull request #1034 from leoloso/rename-fieldResolvers
    Renamed all `FieldResolvers` to `ObjectTypeFieldResolvers`

--- src/ConditionalOnComponent/CustomPosts/FieldResolvers/ObjectType/CustomPostListUserObjectTypeFieldResolver.php
+++ src/ConditionalOnComponent/CustomPosts/FieldResolvers/ObjectType/CustomPostListUserObjectTypeFieldResolver.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace PoPSchema\Users\ConditionalOnComponent\CustomPosts\FieldResolvers\ObjectType;
 
-use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
-use PoPSchema\CustomPosts\FieldResolvers\ObjectType\AbstractCustomPostListFieldResolver;
 use PoPSchema\Users\TypeResolvers\ObjectType\UserTypeResolver;
+use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
+use PoPSchema\CustomPosts\FieldResolvers\ObjectType\AbstractCustomPostListObjectTypeFieldResolver;
 
-class CustomPostUserListFieldResolver extends AbstractCustomPostListFieldResolver
+class CustomPostListUserObjectTypeFieldResolver extends AbstractCustomPostListObjectTypeFieldResolver
 {
     public function getObjectTypeResolverClassesToAttachTo(): array
     {
@@ -20,10 +20,10 @@ class CustomPostUserListFieldResolver extends AbstractCustomPostListFieldResolve
     public function getSchemaFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
     {
         $descriptions = [
-            'customPosts' => $this->translationAPI->__('Custom posts by the user', 'users'),
-            'customPostCount' => $this->translationAPI->__('Number of custom posts by the user', 'users'),
-            'customPostsForAdmin' => $this->translationAPI->__('[Unrestricted] Custom posts by the user', 'users'),
-            'customPostCountForAdmin' => $this->translationAPI->__('[Unrestricted] Number of custom posts by the user', 'users'),
+            'customPosts' => $this->translationAPI->__('Custom posts by the user', 'pop-users'),
+            'customPostCount' => $this->translationAPI->__('Number of custom posts by the user', 'pop-users'),
+            'customPostsForAdmin' => $this->translationAPI->__('[Unrestricted] Custom posts by the user', 'pop-users'),
+            'customPostCountForAdmin' => $this->translationAPI->__('[Unrestricted] Number of custom posts by the user', 'pop-users'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($objectTypeResolver, $fieldName);
     }
--- src/ConditionalOnComponent/CustomPosts/FieldResolvers/ObjectType/CustomPostUserListObjectTypeFieldResolver.php
+++ src/ConditionalOnComponent/CustomPosts/FieldResolvers/ObjectType/CustomPostUserListObjectTypeFieldResolver.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace PoPSchema\Users\ConditionalOnComponent\CustomPosts\FieldResolvers\ObjectType;
 
-use PoPSchema\Users\TypeResolvers\ObjectType\UserTypeResolver;
 use PoP\ComponentModel\TypeResolvers\ObjectType\ObjectTypeResolverInterface;
-use PoPSchema\CustomPosts\FieldResolvers\ObjectType\AbstractCustomPostListFieldResolver;
+use PoPSchema\CustomPosts\FieldResolvers\ObjectType\AbstractCustomPostListObjectTypeFieldResolver;
+use PoPSchema\Users\TypeResolvers\ObjectType\UserTypeResolver;
 
-class CustomPostListUserFieldResolver extends AbstractCustomPostListFieldResolver
+class CustomPostUserListObjectTypeFieldResolver extends AbstractCustomPostListObjectTypeFieldResolver
 {
     public function getObjectTypeResolverClassesToAttachTo(): array
     {
@@ -20,10 +20,10 @@ class CustomPostListUserFieldResolver extends AbstractCustomPostListFieldResolve
     public function getSchemaFieldDescription(ObjectTypeResolverInterface $objectTypeResolver, string $fieldName): ?string
     {
         $descriptions = [
-            'customPosts' => $this->translationAPI->__('Custom posts by the user', 'pop-users'),
-            'customPostCount' => $this->translationAPI->__('Number of custom posts by the user', 'pop-users'),
-            'customPostsForAdmin' => $this->translationAPI->__('[Unrestricted] Custom posts by the user', 'pop-users'),
-            'customPostCountForAdmin' => $this->translationAPI->__('[Unrestricted] Number of custom posts by the user', 'pop-users'),
+            'customPosts' => $this->translationAPI->__('Custom posts by the user', 'users'),
+            'customPostCount' => $this->translationAPI->__('Number of custom posts by the user', 'users'),
+            'customPostsForAdmin' => $this->translationAPI->__('[Unrestricted] Custom posts by the user', 'users'),
+            'customPostCountForAdmin' => $this->translationAPI->__('[Unrestricted] Number of custom posts by the user', 'users'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($objectTypeResolver, $fieldName);
     }
